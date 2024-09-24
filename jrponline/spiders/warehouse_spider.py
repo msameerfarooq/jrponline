b4535a9364ec66ca005a905dbfd7403326e36885
msameerfarooq <me.sameer.farooq@gmail.com>
2024-09-25 01:09:48 +0500

    Added product number column and made some fixes

--- jrponline/spiders/warehouse_spider.py
+++ jrponline/spiders/warehouse_spider.py
@@ -3,14 +3,15 @@ import csv
 import scrapy
 import logging
 import traceback
+import urllib.parse
 from bs4 import BeautifulSoup
 
-USERNAME    = "username"
-PASSWORD    = "password"
-AUTH_ENABLE = False
+USERNAME    = "ICHPAR"
+PASSWORD    = "@IPftw321"
+AUTH_ENABLE = True
 
-ERROR_LOG_FILE      = "err1or.log"
-SCRAPPED_DATA_FILE  = "scrapData.csv"
+ERROR_LOG_FILE      = "error.log"
+SCRAPPED_DATA_FILE  = "scrap_data.csv"
 
 
 class WareHouseScraper(scrapy.Spider):
@@ -23,7 +24,7 @@ class WareHouseScraper(scrapy.Spider):
         # This mode('w') opens a file for writing. If the file exists, its contents are overwritten. If it doesn’t exist, a new file is created.
         self.csv_file = open(SCRAPPED_DATA_FILE, "w", newline="")
         self.csv_writer = csv.writer(self.csv_file)
-        self.csv_writer.writerow(["Category Name", "Product Category", "Product Name", "MSRP Price", "Our Price", "Your Price", "In Stick", "URL"])
+        self.csv_writer.writerow(["Category Name", "Product Category", "Product Name", "Product Number", "MSRP Price", "Our Price", "Your Price", "In Stock", "URL"])
         
         self.log_file = open(ERROR_LOG_FILE, "w", newline="")
 
@@ -34,7 +35,7 @@ class WareHouseScraper(scrapy.Spider):
                                             callback=self.parse_category)
         else:
             yield response.follow(response.url, callback=self.parse_category)
-            
+                        
     # Extract all categories from the homepage
     def parse_category(self, response):
         if AUTH_ENABLE:
@@ -76,7 +77,7 @@ class WareHouseScraper(scrapy.Spider):
             except Exception as e:
                 self.log_file.write("parse_sub_category | Error parsing href : " + href)
                 self.log_file.write(traceback.format_exc())
-                
+                            
     # Get the count of total pages
     def parse_pages_of_sub_category(self, response, category_name, sub_category):
         # Try to find the page number if pagination exist
@@ -86,7 +87,7 @@ class WareHouseScraper(scrapy.Spider):
         
         # Paginations doesn't exist
         except:
-            print("Paginations doesn't exist as it is unable to retrieve total page number from string : ", last_page_button, " , so it'll now only crawl the first page!")
+            print("Paginations doesn't exist as it is unable to retrieve total page number from string : ", last_page_button, ", so it'll now only crawl the first page!")
             yield response.follow(response.url + '&page=', callback=self.parse_list_of_products, cb_kwargs={'category_name': category_name, 'sub_category': sub_category})
         
         else:
@@ -112,8 +113,9 @@ class WareHouseScraper(scrapy.Spider):
     # Extract details of product
     def parse_product(self, response, category_name, sub_category, product_name):
         try:
-            msrp_price = self.retrieve_value(response, '//span[@id="strike"]/text()')
-            our_price = self.retrieve_value(response, '//td[contains(text(), "Our Price:")]/parent::tr/td[2]/text()')
+            product_number = self.extract_product_number(response.url)
+            msrp_price = self.retrieve_value(response, '//td[contains(text(), "MSRP Price:")]/parent::tr/td[2]/span/text()')
+            our_price = self.retrieve_value(response, '//td[contains(text(), "Our Price:")]/parent::tr/td[2]/span/text()')
             your_price = self.retrieve_value(response, '//td[contains(text(), "Your Price:")]/parent::tr/td[2]/text()')
             in_stock_status = self.retrieve_value(response, '//td[contains(text(), "In Stock")]/text()')
             product_url = response.url
@@ -121,10 +123,23 @@ class WareHouseScraper(scrapy.Spider):
             self.log_file.write("parse_product | Error in fetching product details : " + response.url)
             self.log_file.write(traceback.format_exc())
         else:
-            self.csv_writer.writerow([category_name, sub_category, product_name, msrp_price, our_price, your_price, in_stock_status, product_url])
+            self.csv_writer.writerow([category_name, sub_category, product_name, product_number, msrp_price, our_price, your_price, in_stock_status, product_url])
           
     def retrieve_value(self, response, xpath):
         value = response.xpath(xpath).get()
         if value:
             return value.strip()
         return None
+    
+    def extract_product_number(self, url):
+        # Parse the URL using urlparse
+        parsed_url = urllib.parse.urlparse(url)
+
+        # Extract the query string
+        query_string = parsed_url.query
+
+        # Split the query string into key-value pairs
+        params = urllib.parse.parse_qs(query_string)
+
+        encoded_value = params.get('pn', [])  # Use get to handle potential missing key
+        return urllib.parse.unquote(encoded_value[0] if encoded_value else "")  # Handle empty list
